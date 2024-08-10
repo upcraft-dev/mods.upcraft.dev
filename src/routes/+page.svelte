ee5f6dfc69b9b25d564f27782212f3f80b12b277
Up <upcraftedlp@gmail.com>
2024-08-10 08:43:46 +0200

    add placeholder content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center flex-col h-screen justify-center gap-4">
+    <a href="https://upcraft.dev/links/curseforge" target="_blank">Curseforge</a>
+    <a href="https://upcraft.dev/links/modrinth" target="_blank">Modrinth</a>
+    <a href="https://github.com/Up-Mods/repositories" target="_blank" rel="noreferrer">GitHub</a>
+    <hr class="w-64">
+    <a href="https://mods.upcraft.dev/wiki" target="_blank">Wiki</a>
+    <!-- <a href="/dev/versions" class="no-underline tracking-wider">\\dev: versions</a> -->
+    <div class="max-h-72 flex-1"><!-- placeholder div until there is actual content --></div>
+</div>
