dcb9c34f630bdee3249029741ec0dec92e9c645f
Up <upcraftedlp@gmail.com>
2024-08-10 08:56:40 +0200

    fix github link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="flex items-center flex-col h-screen justify-center gap-4">
     <a href="https://upcraft.dev/links/curseforge" target="_blank">Curseforge</a>
     <a href="https://upcraft.dev/links/modrinth" target="_blank">Modrinth</a>
-    <a href="https://github.com/Up-Mods/repositories" target="_blank" rel="noreferrer">GitHub</a>
+    <a href="https://github.com/orgs/Up-Mods/repositories" target="_blank" rel="noreferrer">GitHub</a>
     <hr class="w-64">
     <a href="https://mods.upcraft.dev/wiki" target="_blank">Wiki</a>
     <!-- <a href="/dev/versions" class="no-underline tracking-wider">\\dev: versions</a> -->
